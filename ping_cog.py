01bc6bbcf238630ed81d41d526bee4ef49f76eba
Dan Robbins <drobbins@terpmail.umd.edu>
2023-03-16 13:43:52 -0400

    Fix ping cog

--- ping_cog.py
+++ ping_cog.py
@@ -7,4 +7,4 @@ class ping_cog(commands.Cog):
     @commands.command(name='ping')
     async def _ping(self, ctx: commands.Context):
         m = await ctx.channel.send("Ping?")
-        m.edit("Pong! Latency is {}ms. API Latency is {}ms".format(round((m.created_at - ctx.message.created_at).total_seconds() * 1000), round(self.bot.latency * 1000)))
+        await m.edit(content="Pong! Latency is {}ms. API Latency is {}ms".format(round((m.created_at - ctx.message.created_at).total_seconds() * 1000), round(self.bot.latency * 1000)))
