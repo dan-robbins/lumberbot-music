379f7f8e675cb4d2c11c03cb97733081004bda85
Dan Robbins <drobbins@terpmail.umd.edu>
2023-03-16 14:24:58 -0400

    Fix DM cog

--- dm_cog.py
+++ dm_cog.py
@@ -12,5 +12,5 @@ class dm_cog(commands.Cog):
         if message.author == self.bot.user:
             return
         
-        if not message.author.id == self.bot.user.id and not message.author.id == self.owner_id and message.channel.type == "private":
-            await self.bot.get_user(self.owner_id).send("DM recieved from {} at {}\nContent: {}".format(message.author.name, datetime.now().strftime("%d/%m/%Y %H:%M:%S"), message.content))
+        if not message.author.id == self.bot.user.id and not message.author.id == self.owner_id and str(message.channel.type) == "private":
+            await self.bot.get_user(self.owner_id).send("DM recieved from {}#{} at {}\nContent: {}".format(message.author.name, message.author.discriminator, datetime.now().strftime("%m/%d/%Y %H:%M:%S"), message.content))
