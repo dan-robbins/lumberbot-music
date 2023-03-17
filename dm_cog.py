4503f298229467f57e4594840e0fe48166690e25
Dan Robbins <drobbins@terpmail.umd.edu>
2023-03-16 21:28:42 -0400

    Update dm cog

--- dm_cog.py
+++ dm_cog.py
@@ -1,4 +1,5 @@
 import discord
+import re
 from discord.ext import commands
 from datetime import datetime
 
@@ -6,11 +7,19 @@ class dm_cog(commands.Cog):
     def __init__(self, bot: commands.Bot, owner_id: int):
         self.bot = bot
         self.owner_id = owner_id
+        self.id_regex = re.compile(r"\d{18}")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author == self.bot.user:
             return
         
-        if not message.author.id == self.bot.user.id and not message.author.id == self.owner_id and str(message.channel.type) == "private":
-            await self.bot.get_user(self.owner_id).send("DM recieved from {}#{} at {}\nContent: {}".format(message.author.name, message.author.discriminator, datetime.now().strftime("%m/%d/%Y %H:%M:%S"), message.content))
+        if str(message.channel.type) == "private" and not message.author.id == self.bot.user.id and not message.author.id == self.owner_id:
+            await self.bot.get_user(self.owner_id).send("DM recieved from {}#{}/{} at {}\nContent: {}".format(message.author.name, message.author.discriminator, message.author.id, datetime.now().strftime("%m/%d/%Y %H:%M:%S"), message.content))
+
+        if str(message.channel.type) == "private" and message.author.id == self.owner_id and self.id_regex.match(message.content) is not None:
+            if(len(message.content) > 19):
+                reply_id = int(message.content[0:18])
+                await self.bot.get_user(reply_id).send(message.content[19:])
+            else:
+                message.channel.send("Please include a message to send")
